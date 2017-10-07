fbce0d84af7e1761594b99e9f9084c2a51f79eec
Ian McAulay <ianmcaulay@airbears2-10-142-80-122.airbears2.1918.berkeley.edu>
2017-10-07 16:16:47 -0700

    Cleaned code

--- fb_api.py
+++ fb_api.py
@@ -3,43 +3,30 @@ import urllib
 import requests
 import pdb
 
-token = "EAACEdEose0cBAFb8tXvSvwwkpjUZAwhTW6pIIItS8dZBbWHumwykvNjBIoAzn1EcQUn5TPpMUK5QbGranREagcROWGIuft0UZAO8SEKsTScMTdf0bHIFHxEQ06IRZA5LBV9JxZCItICPh0Jg7VAPvQPRBviKPrT0PZAKwZARkBYmQUUY96H1K54xiWhgb4zik4awBvjSDKyFQZDZD"
+# Ian's token
+#token = "EAACEdEose0cBAFb8tXvSvwwkpjUZAwhTW6pIIItS8dZBbWHumwykvNjBIoAzn1EcQUn5TPpMUK5QbGranREagcROWGIuft0UZAO8SEKsTScMTdf0bHIFHxEQ06IRZA5LBV9JxZCItICPh0Jg7VAPvQPRBviKPrT0PZAKwZARkBYmQUUY96H1K54xiWhgb4zik4awBvjSDKyFQZDZD"
+# Anish's token
+token = "EAACEdEose0cBAFkaMJJTBnV6U2aUeUxHBeaalVICJgd61EZCX7HieZCUZB4sgrQfNuJ3x07U2T2L765gUbU9sZC362uZAGc7s8D9dIfaeXc1K82wzIwMXumtESvfxp1f7d2SvehwLeCK5qWc7Iuc62njjXeBz20G9B13reUcZAjhntuBzqZCPs5jPlQE72CQevw7ZBLaYRZB0uQZDZD"
 
 graph = facebook.GraphAPI(access_token=token)
-fields = 'link,images'
-# This id is currently hardcoded
-post = graph.get_object(id=10154781019457991, fields = fields)
-link = post['images'][0]['source']
-
-def save_image_from_url(url, filename="file01.jpg"):
-
-    resource = urllib.urlopen(url)
-    output = open(filename,"wb")
-    output.write(resource.read())
 
 
+def save_all_photos(folder='all_ian_photos', limit=100):
 
-img = save_image_from_url(link)
-
-fields = 'link'
-ian = graph.get_object(id='me', fields=fields)
-all_photos = graph.get_connections(id='me', connection_name="photos")
-#timeline_url = ian['link']
-#timeline = graph.get_object(timeline_url)
-
-def save_all_photos():
-
-    all_photos = graph.get_connections(id='https://www.facebook.com/ian.mcaulay.79', connection_name='photos')
-    all_photos = all_photos['data']
+    all_photos = graph.get_connections(id='me', connection_name='photos')
+    all_photos = all_photos['data'][:limit]
     for i, photo in enumerate(all_photos):
-        id = photo['id']
-        img = graph.get_object(id=id, fields='images')
-        link = img['images'][0]['source']
-        resource = urllib.urlopen(link)
-        filename = "all_ian_photos/photo" + str(i) + ".jpg"
+        img = get_img_from_fb_photo(photo)
+        filename = folder + "/photo" + str(i) + ".jpg"
         output = open(filename,"wb")
-        output.write(resource.read())
-        #pdb.set_trace()
+        output.write(img)
+
+def get_img_from_fb_photo(photo):
+    id = photo['id']
+    img = graph.get_object(id=id, fields='images')
+    link = img['images'][0]['source']
+    resource = urllib.urlopen(link)
+    return resource.read()
 
 
 
---
+++ old_code.py
@@ -0,0 +1,20 @@
+
+#fields = 'link,images'
+# This id is currently hardcoded
+#post = graph.get_object(id=10154781019457991, fields = fields)
+#link = post['images'][0]['source']
+
+#img = save_image_from_url(link)
+
+#fields = 'link'
+#ian = graph.get_object(id='me', fields=fields)
+#all_photos = graph.get_connections(id='me', connection_name="photos")
+#timeline_url = ian['link']
+#timeline = graph.get_object(timeline_url)
+
+#def save_image_from_url(url, filename="file01.jpg"):
+
+#    resource = urllib.urlopen(url)
+ #   output = open(filename,"wb")
+  #  output.write(resource.read())
+
