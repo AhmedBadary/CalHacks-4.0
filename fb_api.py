c36058845a4b53854e4896897495489b12597a12
Ian McAulay <ianmcaulay@airbears2-10-142-80-122.airbears2.1918.berkeley.edu>
2017-10-07 17:35:00 -0700

    Adding msft api stuff

--- fb_api.py
+++ fb_api.py
@@ -4,9 +4,9 @@ import requests
 import pdb
 
 # Ian's token
-#token = "EAACEdEose0cBAFb8tXvSvwwkpjUZAwhTW6pIIItS8dZBbWHumwykvNjBIoAzn1EcQUn5TPpMUK5QbGranREagcROWGIuft0UZAO8SEKsTScMTdf0bHIFHxEQ06IRZA5LBV9JxZCItICPh0Jg7VAPvQPRBviKPrT0PZAKwZARkBYmQUUY96H1K54xiWhgb4zik4awBvjSDKyFQZDZD"
+token = "EAACEdEose0cBAPVw3LVFG02zgq98QuLPVmdOlyy9ZCEKVbX6LuHXcVdcZBErcCqPUhHAD4iNxQFWvSExjH5PW1DLBtrB3zuMqKLgf8H3FKlQ3ZBXXfTaSPKbtSCy31BpY87ECmLtVKT6LqYeggkriLX13dHen26sZCtgZCIrPZAtZB79VyxcDWfmyVUU0XtHIvBB5cnKUzZCVEj7aP2mdVKwpTktVbV8NkjbfOkmWiEBpAZDZD"
 # Anish's token
-token = "EAACEdEose0cBAFkaMJJTBnV6U2aUeUxHBeaalVICJgd61EZCX7HieZCUZB4sgrQfNuJ3x07U2T2L765gUbU9sZC362uZAGc7s8D9dIfaeXc1K82wzIwMXumtESvfxp1f7d2SvehwLeCK5qWc7Iuc62njjXeBz20G9B13reUcZAjhntuBzqZCPs5jPlQE72CQevw7ZBLaYRZB0uQZDZD"
+#token = "EAACEdEose0cBAFkaMJJTBnV6U2aUeUxHBeaalVICJgd61EZCX7HieZCUZB4sgrQfNuJ3x07U2T2L765gUbU9sZC362uZAGc7s8D9dIfaeXc1K82wzIwMXumtESvfxp1f7d2SvehwLeCK5qWc7Iuc62njjXeBz20G9B13reUcZAjhntuBzqZCPs5jPlQE72CQevw7ZBLaYRZB0uQZDZD"
 
 graph = facebook.GraphAPI(access_token=token)
 
@@ -25,8 +25,11 @@ def get_img_from_fb_photo(photo):
     id = photo['id']
     img = graph.get_object(id=id, fields='images')
     link = img['images'][0]['source']
+    pdb.set_trace()
     resource = urllib.urlopen(link)
     return resource.read()
+
+
 
 
 
---
+++ msft_api.py
@@ -0,0 +1,44 @@
+import httplib, urllib, base64, json
+
+subscription_key = "14ae1355d65940dcb149b368e87d1ab9"
+uri_base = 'westcentralus.api.cognitive.microsoft.com'
+
+# Request headers.
+headers = {
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': subscription_key,
+}
+
+# Request parameters.
+params = urllib.urlencode({
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+})
+
+
+
+def get_msft_data(url):
+
+    # The URL of a JPEG image to analyze.
+    body = "{'url':'" + url + "'}"
+    body = "{'url':'https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg'}"
+
+    try:
+        # Execute the REST API call and get the response.
+        conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+        conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
+        response = conn.getresponse()
+        # 'data' contains the JSON data. The following formats the JSON data for display.
+        data = response.read()
+        conn.close()
+        return data 
+
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+
+
+
+
+
